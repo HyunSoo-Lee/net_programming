350e08ba59de5a0767fc19defc1dda447c1b4f50
HyunSoo Lee <oceanyum@gmail.com>
2023-04-13 15:22:45 +0900

    last midterm prac

--- MIDTERM_Prac/String.py
+++ MIDTERM_Prac/String.py
@@ -5,6 +5,7 @@ print(str *5)
 print(str[:3])
 print(str[-3:])
 print(str.upper())
+print('\n\n')
 
 lst = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'I', 'o', 'T']
 lst.append('!')
@@ -13,3 +14,50 @@ del lst[4]
 print(lst)
 lst.insert(4, 'a')
 print(lst)
+print(''.join(lst))
+print(lst)
+lst.sort(reverse=True)
+print(lst)
+print('\n\n')
+
+str = 'https://search.naver.com/search.naver?where=nexearch&ie=utf8&query=iot'
+#{'where':'nexearch', 'ie':'utf8', 'query':'iot'}
+
+lst = str.split('?')
+print(lst)
+
+spllst = lst[1].split('&')
+print(spllst)
+
+x = []
+for i in range(3):
+    x.append(spllst[i].split('='))
+print(x)
+
+answer = dict()
+for i in range(3):
+    answer[x[i][0]] = x[i][1]
+print(answer)
+print('\n\n')
+
+
+
+class MyComplex:
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+def Calculator(complex1, complex2):
+    a = complex1.real
+    b = complex1.imaginary
+    c = complex2.real
+    d = complex2.imaginary
+    real_cal = a*c - b*d
+    imag_cal = a*d + b*c
+    result = f'{real_cal} + {imag_cal}i' 
+    return  result
+
+complex_1 = MyComplex(3, -4)
+complex_2 = MyComplex(-5,2)
+print(Calculator(complex_1, complex_2))
+
